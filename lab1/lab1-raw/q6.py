7492a754944921965dd50ea51e9d385c7c308eed
Amir Alimohammadi <45646480+alimohammadiamirhossein@users.noreply.github.com>
2025-09-12 09:50:18 -0700

    Add files via upload

---
+++ lab1/lab1-raw/q6.py
@@ -0,0 +1,29 @@
+import random
+
+# Todo 1: Set a seed
+
+# Todo 2: weighted_sum using for loop
+def weighted_sum_1(weights, values):
+    """Calculate weighted sum using for loop"""
+    total = 0
+    return total
+
+# Todo 3: weighted_sum using list comprehension
+def weighted_sum_2(weights, values):
+    """Calculate weighted sum using list comprehension"""
+    return ...
+
+
+# Define main function
+def main():
+    n = 10**6  # Number of elements
+    weights, values = [], []
+    # Todo 4: Generate random weights and values
+    
+    print(weighted_sum_1(weights, values))
+
+    print(weighted_sum_2(weights, values))
+
+
+if __name__ == "__main__":
+    main()
